608ca9b535993d7d483bcd72bf1f1fb4d6d8d072
Dana Yakoobinsky <dyakoobinsky@gmail.com>
2015-02-05 14:26:50 -0800

    creating new posts from sms data

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -1,28 +1,38 @@
 module Api
   class PostsController < ApplicationController
     before_action :require_signed_in!
+    skip_before_action :verify_authenticity_token, only: [:create]
 
     def create
       puts params
       logger.debug "Post Params: #{params.to_json}"
-      @post = Post.new(post_params)
-      if @event.save
-        render :show
-      else
-        render json: @post.errors.full_messages, status: 422
+      post_params = {
+        body: params["Body"],
+        event_id: Event.find_by_phone_number(params["To"]).id
+      }
+
+      num_pics = params["NumMedia"].to_i
+      (0..num_pics).each do |i|
+        post_params["picture_url"] = params["MediaUrl".concat(i.to_s)]
+        @post = Post.new(post_params)
+        if @post.save
+          # render :show
+        else
+          render json: @post.errors.full_messages, status: 422
+        end
       end
     end
 
     def destroy
-      @post = Event.find(params[:id])
+      @post = Post.find(params[:id])
       @post.delete
       render :show
     end
 
-  private
-
-    def post_params
-      params.require(:post).permit(:title, :event_date)
-    end
-  end
+  # private
+  #
+  #   def post_params
+  #     params.require(:post).permit(:title, :event_date)
+  #   end
+  # end
 end
