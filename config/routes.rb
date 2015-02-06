e59c9d1f4fc8294a5856e08a602474e78490e7ec
Dana Yakoobinsky <dyakoobinsky@gmail.com>
2015-02-06 11:27:38 -0800

    reroute visitor to sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/root'
   root to: "static_pages#root"
   resources :users, except: [:edit, :update]
   resource :session, only: [:new, :create, :destroy]
-
   namespace :api, defaults: { format: :json } do
    resources :events, except: [:new, :edit]
    resources :posts, only: [:create, :destroy]
