b415a3dcba68c9d11b798da959ed0d77ef34690b
Dana Yakoobinsky <dyakoobinsky@gmail.com>
2015-02-06 17:33:45 -0800

    Added footer on sign up page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
-  before_action :require_signed_in!
-  
+  before_action :require_signed_in!, only: [:root]
+
   def root
     render :root
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'static_pages/root'
+  get 'static_pages/about'
+  get 'static_pages/contact'
   root to: "static_pages#root"
   resources :users, except: [:edit, :update]
   resource :session, only: [:new, :create, :destroy]
