11637375fcbebe795f6d9cb35c22c09be6a08388
Dana Yakoobinsky <dyakoobinsky@gmail.com>
2015-02-08 21:36:27 -0800

    made event show page a slideshow

--- app/assets/javascripts/views/events/show_event.js
+++ app/assets/javascripts/views/events/show_event.js
@@ -1,14 +1,22 @@
-Corky.Views.EventShow = Backbone.View.extend({
+Corky.Views.EventShow = Backbone.CompositeView.extend({
   template: JST['events/show'],
+  templatePost: JST['posts/show_big'],
   className: "show-page",
 
   initialize: function(){
     this.listenTo(this.model, "sync change", this.render)
     this.listenTo(this.model.posts(), "sync", this.render)
+    this.listenTo(this.model.posts(), "add", this.addPostView);
+    this.listenTo(this.model.posts(), "remove", this.removePost)
+    this.model.posts().each(this.addPostView.bind(this));
     var pusher = new Pusher(PUSHER_APP_ID);
     var channelName = 'event'.concat(this.model.id);
     var channel = pusher.subscribe(channelName);
     var event = this.model;
+    this.posts = this.model.posts().models;
+    this.postCount = 0;
+    // this.slides = this.$(".post");
+    // this.postLength = this.slides.length - 1;
     channel.bind('fetchPosts',
       function() {
         event.fetch();
@@ -17,43 +25,46 @@ Corky.Views.EventShow = Backbone.View.extend({
   },
 
 
-  //
-  //
-  // slideshow: function () {
-  //     var counter = 0;
-  //     var slides = this.$(".posts");
-  //     var slidesLen = slides.length - 1;
-  //     return function () {
-  //       setInterval(function () {
-  //           if (counter === 0) {
-  //             slides.eq(slidesLen).addClass("col-md-4");
-  //             slides.eq(slidesLen).removeClass("on-display");
-  //             slides.eq(counter).removeClass("col-md-4");
-  //             slides.eq(counter).addClass("on-display");
-  //             counter += 1;
-  //           } else if (counter === slidesLen) {
-  //               slides.eq(counter-1).addClass("col-md-4");
-  //               slides.eq(counter-1).removeClass("on-display");
-  //               slides.eq(counter).removeClass("col-md-4");
-  //               slides.eq(counter).addClass("on-display");
-  //               counter = 0;
-  //           } else {
-  //             slides.eq(counter-1).addClass("col-md-4");
-  //             slides.eq(counter-1).removeClass("on-display");
-  //             slides.eq(counter).removeClass("col-md-4");
-  //             slides.eq(counter).addClass("on-display");
-  //             counter += 1;
-  //           }
-  //       }, 2000);
-  //     };
-  // },
+  addPostView: function(post){
+    var postItemShow = new Corky.Views.PostItemView({ model: post, collection: this.collection });
+    this.addSubview(".posts", postItemShow.render());
+  },
+
+  removePost: function(post){
+    var selector = ".posts";
+    var subRemove = _(this.subviews(selector)).find(function(sub){return sub.model === post} );
+    this.removeSubview(selector, subRemove);
+  },
+
+
+  slideshow: function () {
+    this.$(".posts").fadeOut("slow", function(){
+      var postTemp;
+      var currentPost;
+      currentPost = this.posts[this.postCount];
+      postTemp = this.templatePost({post: currentPost});
+      this.$el.html(postTemp).hide().fadeIn()
+      if (this.postCount === 0) {
+        this.postCount += 1;
+      } else if (this.postCount === this.postLength-1) {
+          this.postCount = 0;
+      } else {
+          this.postCount += 1;
+      }
+      setTimeout(function(){
+        this.$(".on-display").fadeOut("slow", this.render.bind(this))
+      }.bind(this), 6000)
+    }.bind(this));
+
+  },
 
 
   render: function(){
     var content = this.template({event: this.model});
-    this.$el.html(content);
-    // var slide = this.slideshow();
-    // slide();
+    this.$el.html(content).hide().fadeIn();
+    this.attachSubviews();
+    this.postLength = this.posts.length;
+    setTimeout(this.slideshow.bind(this),3000);
     return this;
   }
 
